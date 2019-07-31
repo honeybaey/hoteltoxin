46eb7546ef47297bcb23d7edba2bb72efac75da8
honeybaey <honeybaey@gmail.com>
2019-08-01 01:34:02 +0300

    added item quantity dropdown

--- src/index.js
+++ src/index.js
@@ -4,3 +4,5 @@ import './plugins/maskPlugin';
 import '../node_modules/jquery-mask-plugin/dist/jquery.mask.min';
 import '../node_modules/air-datepicker/dist/js/datepicker.min';
 import './plugins/airDatepicker';
+import '../node_modules/item-quantity-dropdown/lib/item-quantity-dropdown.min.js';
+import './plugins/itemDropdown';
