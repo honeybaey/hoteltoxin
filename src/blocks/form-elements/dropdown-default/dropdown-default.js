09953abee9bf86894f2cc5290c2c32fd69783f1f
honeybaey <honeybaey@gmail.com>
2019-09-03 22:15:59 +0300

    has been changed structure

--- src/blocks/form-elements/dropdown-default/dropdown-default.js
+++ src/blocks/form-elements/dropdown-default/dropdown-default.js
@@ -1,20 +1,15 @@
-$(document).ready(() => {
-  $('.iqdropdown').iqDropdown({});
+$( function() {
+  $("input[name='guest']").guest({
+      categoryNames: ["Взрослые", "Дети", "Младенцы"],
+      categoryValues: false,
+      minValue: 0,
+      maxValue: 10,
+      closeOnOutsideClick: false,
+      showText: true,
+      delimiter: ", ",
+      align: "left",
+      fade: true,
+      useDisplay: false,
+      showZero: false,
+  });
 });
-
-/* function num2str(n, text_forms) {  
-  n = Math.abs(n) % 100; var n1 = n % 10;
-  if (n > 10 && n < 20) { return text_forms[2]; }
-  if (n1 > 1 && n1 < 5) { return text_forms[1]; }
-  if (n1 == 1) { return text_forms[0]; }
-  return text_forms[2];
-}
-
-$('#item1').html('1 '+num2str(1, ['минута', 'минуты', 'минут']));
-$('#item2').html('2 '+num2str(2, ['минута', 'минуты', 'минут']));
-$('#item3').html('5 '+num2str(5, ['минута', 'минуты', 'минут']));
-
-$('.iqdropdown').on('change',function(){
-  val = num2str($(this).val(), ['гость', 'гостя', 'гостей']);
-  $('.item1').text(val);
-}); */  // склонение счетчика
