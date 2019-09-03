6c4d5abc6e634ed8ea404e7fba6577d1cfe1d025
honeybaey <honeybaey@gmail.com>
2019-09-03 22:14:25 +0300

    added dropdown comfort

---
+++ src/blocks/form-elements/dropdown-comfort/dropdown-comfort.js
@@ -0,0 +1,15 @@
+$( function() {
+  $("input[name='NCS']").NCS({
+      categoryNames: ["Спальни", "Кровати", "Ванные комнаты"],
+      categoryValues: false,
+      minValue: 0,
+      maxValue: 10,
+      closeOnOutsideClick: true,
+      showText: true,
+      delimiter: ", ",
+      align: "left",
+      fade: true,
+      useDisplay: false,
+      showZero: false,
+  });
+});
