a5498ecba8defe9d83f04ac12d402cbb5cf42ca7
honeybaey <honeybaey@gmail.com>
2019-09-04 22:56:38 +0300

    edited path air-datepicker

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@
 import './scss/styles.scss';
 import './blocks/form-elements/masked-text-field/masked-text-field';
 import '../node_modules/jquery-mask-plugin/dist/jquery.mask.min';
-import '../node_modules/air-datepicker/dist/js/datepicker.min';
+import './blocks/form-elements/date-dropdown/datepicker.min';
 import './blocks/form-elements/date-dropdown/date-dropdown';
-import '../node_modules/item-quantity-dropdown/lib/item-quantity-dropdown.min.js';
+// import '../node_modules/item-quantity-dropdown/lib/item-quantity-dropdown.min.js';
 import './blocks/form-elements/dropdown-default/dropdown-default';
 import './blocks/form-elements/dropdown-default/numberCategorySelectorGuests';
 import './blocks/form-elements/dropdown-comfort/numberCategorySelectorComfort';
