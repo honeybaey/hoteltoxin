14447c1202577ab22febbc72bb74d581b9a5d78c
honeybaey <honeybaey@gmail.com>
2019-10-09 17:56:58 +0300

    restructuring of the project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 // import 'jquery'; при подключении через ProvidePlugin, import в точке входа необязателен
 import './scss/styles.scss';
-import './blocks/form-elements/masked-text-field/masked-text-field';
+import './blocks/masked-text-field/masked-text-field';
 import '../node_modules/jquery-mask-plugin/dist/jquery.mask.min';
-import './blocks/form-elements/date-dropdown/datepicker.min';
-import './blocks/form-elements/date-dropdown/date-dropdown';
-import './blocks/form-elements/dropdown-default/dropdown-default';
-import './blocks/form-elements/dropdown-default/numberCategorySelectorGuests';
-import './blocks/form-elements/dropdown-comfort/dropdown-comfort';
-import './blocks/form-elements/dropdown-comfort/numberCategorySelectorComfort';
-import './blocks/form-elements/expandable-checkbox-list/expandable-checkbox-list';
-import './blocks/form-elements/like-button/like-button';
-import './blocks/form-elements/range-slider/range-slider.js';
-import './blocks/form-elements/range-slider/nouislider';
+import './blocks/date-dropdown/datepicker.min';
+import './blocks/date-dropdown/date-dropdown';
+import './blocks/dropdown-default/dropdown-default';
+import './blocks/dropdown-default/numberCategorySelectorGuests';
+import './blocks/dropdown-comfort/dropdown-comfort';
+import './blocks/dropdown-comfort/numberCategorySelectorComfort';
+import './blocks/expandable-checkbox-list/expandable-checkbox-list';
+import './blocks/like-button/like-button';
+import './blocks/range-slider/range-slider.js';
+import './blocks/range-slider/nouislider';
