21a6d4921aa2c3264a293dad4595a9e850f733d8
honeybaey <honeybaey@gmail.com>
2019-11-27 19:11:36 +0300

    formatted code

--- src/blocks/date-dropdown/date-dropdown.js
+++ src/blocks/date-dropdown/date-dropdown.js
@@ -1,26 +1,27 @@
-$('#date-dropdown').datepicker();
-$('#date-dropdown_start').datepicker();
-$('#date-dropdown_end').datepicker();
-$('#filter-date-dropdown').datepicker();
+$("#date-dropdown").datepicker();
+$("#date-dropdown_start").datepicker();
+$("#date-dropdown_end").datepicker();
+$("#filter-date-dropdown").datepicker();
 
-var applyButton = '<span class="datepicker--button datepicker--button__apply" data-action="hide">Применить</span>'; 
-$('.datepicker--button[data-action="clear"]').each(function( index ) { $(applyButton).insertBefore($(this)); });
+var applyButton =
+  '<span class="datepicker--button datepicker--button__apply" data-action="hide">Применить</span>';
+$('.datepicker--button[data-action="clear"]').each(function(index) {
+  $(applyButton).insertBefore($(this));
+});
 
-var $start = $('#date-dropdown_start'),
-    $end = $('#date-dropdown_end');
+var $start = $("#date-dropdown_start"),
+  $end = $("#date-dropdown_end");
 
 $start.datepicker({
-    onSelect: function (fd, date) {
-        $end.data('datepicker')
-                .update('minDate', date);
+  onSelect: function(fd, date) {
+    $end.data("datepicker").update("minDate", date);
 
-        $end.focus();
-    }
-})
+    $end.focus();
+  }
+});
 
 $end.datepicker({
-    onSelect: function (fd, date) {
-        $start.data('datepicker')
-                .update('maxDate', date)
-    }
-})
+  onSelect: function(fd, date) {
+    $start.data("datepicker").update("maxDate", date);
+  }
+});
