1972631bdd51c45c0a74ceda74197dc0a93b456c
honeybaey <honeybaey@gmail.com>
2019-11-27 20:13:02 +0300

    added rate-button

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 // import 'jquery'; при подключении через ProvidePlugin, import в точке входа необязателен
-import './scss/styles.scss';
-import './utils';
-import './blocks/masked-text-field/masked-text-field';
-import '../node_modules/jquery-mask-plugin/dist/jquery.mask.min';
-import './blocks/date-dropdown/datepicker.min';
-import './blocks/date-dropdown/date-dropdown';
-import './blocks/dropdown/dropdown';
+import "./scss/styles.scss";
+import "./utils";
+import "./blocks/masked-text-field/masked-text-field";
+import "../node_modules/jquery-mask-plugin/dist/jquery.mask.min";
+import "./blocks/date-dropdown/datepicker.min";
+import "./blocks/date-dropdown/date-dropdown";
+import "./blocks/dropdown/dropdown";
+import "./blocks/rate-button/rate-button";
 // import './blocks/expandable-checkbox-list/expandable-checkbox-list';
 // import './blocks/like-button/like-button';
-import './blocks/range-slider/range-slider.js';
-import './blocks/range-slider/nouislider';
-import './blocks/room-card/room-card';
-import './blocks/pie-chart/pie-chart';
+import "./blocks/range-slider/range-slider.js";
+import "./blocks/range-slider/nouislider";
+import "./blocks/room-card/room-card";
+import "./blocks/pie-chart/pie-chart";
