a420f9f59a49e23f190a9ce8a1942c6991fb1c91
honeybaey <honeybaey@gmail.com>
2019-11-28 19:52:18 +0300

    added function wrapper

--- src/blocks/expandable-checkbox-list/expandable-checkbox-list.js
+++ src/blocks/expandable-checkbox-list/expandable-checkbox-list.js
@@ -1,15 +1,17 @@
-let expanded = false;
-let elem = document.querySelector('.select-box');
+$(".expandable-checkbox_wrapper").each(function() {
+  let expanded = false;
+  let selectBox = document.querySelector(".select-box");
 
-elem.onclick = showCheckboxes;
-
-function showCheckboxes() {
-  let checkboxes = document.getElementById("checkboxes");
-  if (!expanded) {
-    checkboxes.style.display = "block";
-    expanded = true;
-  } else {
-    checkboxes.style.display = "none";
-    expanded = false;
+  function showCheckboxes() {
+    let checkboxes = document.querySelector(".expandable-checkbox__items");
+    if (!expanded) {
+      checkboxes.style.display = "block";
+      expanded = true;
+    } else {
+      checkboxes.style.display = "none";
+      expanded = false;
+    }
   }
-}
+
+  selectBox.onclick = showCheckboxes;
+});
