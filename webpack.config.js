c1ec8d2c8afdea3b9317f999aedc15a6c5d95f8f
honeybaey <honeybaey@gmail.com>
2020-09-23 15:15:43 +0300

    edited styles

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,18 @@ const webpack = require("webpack");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 const { baseUrl } = require("./config");
+
+const isDev = process.env.NODE_ENV === "development";
+const isProd = !isDev;
 
 module.exports = {
   entry: ["./src/index.js"],
   output: {
     path: path.resolve(__dirname, "./dist"),
-    filename: "./js/main.js"
+    filename: "./js/main.js",
     // publicPath: '/dist',
   },
   /* resolve: {
@@ -20,45 +23,54 @@ module.exports = {
     }
   }, */
   devServer: {
-    openPage: ["landing-page.html"]
+    openPage: ["landing-page.html"],
   },
   module: {
     rules: [
       {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: isDev,
+              reloadAll: true,
+            },
+          },
+          "css-loader",
+          "postcss-loader",
+        ],
+      },
+      {
         test: /\.scss$/,
         // use:  [  'style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
         use: [
+          /*           {
+            loader: "style-loader",
+          }, */
           {
-            loader: "style-loader"
-          },
-          {
-            loader: MiniCssExtractPlugin.loader
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: isDev,
+              reloadAll: true,
+            },
           },
           {
             loader: "css-loader",
             options: {
-              url: true
-            }
+              url: true,
+            },
           },
-          {
-            loader: "postcss-loader",
-            options: {
-              plugins: function() {
-                return [require("autoprefixer")];
-              }
-            }
-          },
-          {
-            loader: "sass-loader"
-          }
-        ]
+          "postcss-loader",
+          "sass-loader",
+        ],
       },
       {
         test: /\.pug$/,
         loader: "pug-loader",
         options: {
-          pretty: true
-        }
+          pretty: true,
+        },
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
@@ -68,11 +80,11 @@ module.exports = {
             options: {
               publicPath: `${baseUrl}img/`,
               name: "[name].[ext]",
-              outputPath: "./img"
-            }
-          }
+              outputPath: "./img",
+            },
+          },
         ],
-        exclude: [/fonts/]
+        exclude: [/fonts/],
       },
       {
         test: /\.(eot|svg|ttf|woff)$/,
@@ -82,57 +94,59 @@ module.exports = {
             options: {
               publicPath: "../fonts/",
               name: "[name].[ext]",
-              outputPath: "./fonts"
-            }
-          }
-        ]
-      }
-    ]
+              outputPath: "./fonts",
+            },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
-      filename: "css/style.css"
+      filename: "css/style.css",
     }),
     new HtmlWebpackPlugin({
       filename: "index.html",
-      template: "./src/pug/index.pug"
+      template: "./src/pug/index.pug",
     }),
     new HtmlWebpackPlugin({
       filename: "ui-kit.html",
-      template: "./src/pages/ui-kit/ui-kit.pug"
+      template: "./src/pages/ui-kit/ui-kit.pug",
     }),
     new HtmlWebpackPlugin({
       filename: "landing-page.html",
-      template: "./src/pages/landing-page/landing-page.pug"
+      template: "./src/pages/landing-page/landing-page.pug",
     }),
     new HtmlWebpackPlugin({
       filename: "search-room.html",
-      template: "./src/pages/search-room/search-room.pug"
+      template: "./src/pages/search-room/search-room.pug",
     }),
     new HtmlWebpackPlugin({
       filename: "room-details.html",
-      template: "./src/pages/room-details/room-details.pug"
+      template: "./src/pages/room-details/room-details.pug",
     }),
     new HtmlWebpackPlugin({
       filename: "registration.html",
-      template: "./src/pages/registration/registration.pug"
+      template: "./src/pages/registration/registration.pug",
     }),
     new HtmlWebpackPlugin({
       filename: "login.html",
-      template: "./src/pages/login/login.pug"
+      template: "./src/pages/login/login.pug",
     }),
-    new CopyWebpackPlugin([
-      { from: "./src/img", to: "./img" },
-      { from: "./src/static", to: "./img" }
-    ]),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "./src/img", to: "./img" },
+        { from: "./src/static", to: "./static" },
+      ],
+    }),
     new webpack.ProvidePlugin({
       $: "jquery",
       jQuery: "jquery",
       "window.jQuery": "jquery",
       noUiSlider: "nouislider",
       wNumb: "wnumb",
-      Chartist: "chartist"
-    })
-  ]
+      Chartist: "chartist",
+    }),
+  ],
 };
